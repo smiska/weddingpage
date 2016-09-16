cd8f7cdff29561373adf7a365553dec9e90f4940
miguel47 <saroy.mihaly@gmail.com>
2016-09-16 11:55:03 +0000

    create basic gulp tasks, folder structure

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,31 @@
 
 // include gulp plugins
 var 
-    gulp = require("gulp");
+    gulp = require("gulp"),
+    newer = require("gulp-newer"),
+    imagemin = require("gulp-imagemin");
+   
 
 // file locations 
 
 var
     source = "source/",
-    destination = "build/";
+    destination = "build/",
+    
+    images = {
+        in: source + "images/*.*",
+        out: destination + "images"
+    };
+    
+// copy and minify source images
+
+gulp.task("images", function() {
+    return gulp.src(images.in)
+            .pipe(newer(images.out))
+            .pipe(imagemin())
+            .pipe(gulp.dest(images.out));
+});
+
 
 // gulp default task
 
